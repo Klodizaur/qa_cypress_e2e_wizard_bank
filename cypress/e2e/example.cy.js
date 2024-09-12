3e80842afaa4ba8c889992f81aa85c9cc1dd50f8
“Klaudia <klaudia@atomicweb.design>
2024-09-12 09:27:49 +0100

    Solution

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,51 +1,66 @@
-import { faker } from '@faker-js/faker';
 /// <reference types='cypress' />
+import { faker } from '@faker-js/faker';
 
 describe('Bank app', () => {
   const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
   const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
-  const balance = depositAmount - withdrawAmount;
-  const user = 'Harry Potter';
-  const accountNumber = '1004';
+  const balance = parseInt(depositAmount) - parseInt(withdrawAmount);
+  const user = 'Hermoine Granger';
+  const accountNumber = '1002';
+  const accountNumber2 = '1003';
 
   before(() => {
-    cy.visit('/');
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
   });
 
-  it('should provide the ability to work with bank account', () => {
+  it('should provide the ability to work with Hermiones bank account', () => {
+    // Log in as Hermoine Granger
     cy.contains('.btn', 'Customer Login').click();
     cy.get('[name="userSelect"]').select(user);
     cy.contains('.btn', 'Login').click();
 
+    // Select the first account
+    cy.get('[name="accountSelect"]').select(accountNumber);
+
+    // Verify account details
     cy.contains('[ng-hide="noAccount"]', 'Account Number')
       .contains('strong', accountNumber)
       .should('be.visible');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
       .contains('strong', '0')
       .should('be.visible');
-    cy.contains('.ng-binding', 'Dollar')
-      .should('be.visible');
+    cy.contains('.ng-binding', 'Pound').should('be.visible');
 
+    // Perform deposit
     cy.get('[ng-click="deposit()"]').click();
     cy.get('[placeholder="amount"]').type(depositAmount);
     cy.contains('[type="submit"]', 'Deposit').click();
 
-    cy.get('[ng-show="message"]')
-      .should('contain', 'Deposit Successful');
+    // Verify balance after deposit
+    cy.get('[ng-show="message"]').should('contain', 'Deposit Successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
       .contains('strong', depositAmount)
       .should('be.visible');
 
+    // Perform withdrawal
     cy.get('[ng-click="withdrawl()"]').click();
-    cy.contains('[type="submit"]', 'Withdraw')
-      .should('be.visible');
+    cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
     cy.get('[placeholder="amount"]').type(withdrawAmount);
     cy.contains('[type="submit"]', 'Withdraw').click();
 
-    cy.get('[ng-show="message"]')
-      .should('contain', 'Transaction successful');
+    // Verify balance after withdrawal
+    cy.get('[ng-show="message"]').should('contain', 'Transaction successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
       .contains('strong', balance)
       .should('be.visible');
-  });
+
+    cy.get('[ng-click="transactions()"]').click();
+      cy.get('.fixedTopBox > [style="float:left"]').click();
+      cy.get('[name="accountSelect"]').select(accountNumber2);
+      cy.get('[ng-class="btnClass1"]').click();
+      cy.get('.marTop').should('not.contain', '#anchor0 > :nth-child(3)');
+
+      cy.get('.logout').click();
+      cy.get('.mainhdr').should('not.contain', 'logout');
+    });
 });
--- cypress/e2e/hermioneAccount.spec.js
+++ cypress/e2e/hermioneAccount.spec.js
@@ -1,11 +1,20 @@
 /// <reference types='cypress' />
 
-describe('Bank app', () => {
-  before(() => {
-    cy.visit('/');
-  });
+// Function to perform a deposit
+export function performDeposit(amount) {
+  cy.get('[ng-click="deposit()"]').click();
+  cy.get('[placeholder="amount"]').type(amount);
+  cy.contains('[type="submit"]', 'Deposit').click();
 
-  it('should provide the ability to work with Hermione\'s bank account', () => {
+  cy.get('[ng-show="message"]').should('contain', 'Deposit Successful');
+}
 
-  });
-});
+// Function to perform a withdrawal
+export function performWithdrawal(amount) {
+  cy.get('[ng-click="withdrawl()"]').click();
+  cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
+  cy.get('[placeholder="amount"]').type(amount);
+  cy.contains('[type="submit"]', 'Withdraw').click();
+
+  cy.get('[ng-show="message"]').should('contain', 'Transaction successful');
+}
